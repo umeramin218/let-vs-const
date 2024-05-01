f0027bb66c8ec645628429c9fe946023d45b365c
umeramin218 <160252422+umeramin218@users.noreply.github.com>
2024-05-01 18:44:07 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var currentBalance = 100; // this can be change 
+var accountNumber = 1234; // this can't be change 
+currentBalance = 150; // this cxan be change again
+var um = ;
---
+++ main.ts
@@ -0,0 +1,4 @@
+let currentBalance = 100 // this can be change 
+const accountNumber = 1234 // this can't be change 
+
+currentBalance = 150 // this cxan be change agais
